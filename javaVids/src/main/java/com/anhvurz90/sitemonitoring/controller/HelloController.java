10d6fea08e2e5455399ff4ccd9e9185f4d2522d0
anhvurz90 <anhvurz90@gmail.com>
2017-08-09 15:34:12 +0700

    Add JavaVids: JSF & PrimeFaces & Spring tutorial 5: Spring Integration

--- javaVids/src/main/java/com/anhvurz90/sitemonitoring/controller/HelloController.java
+++ javaVids/src/main/java/com/anhvurz90/sitemonitoring/controller/HelloController.java
@@ -1,11 +1,23 @@
 package com.anhvurz90.sitemonitoring.controller;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import com.anhvurz90.sitemonitoring.service.HelloSpringService;
 
 @ManagedBean
 public class HelloController {
 
+    @ManagedProperty("#{helloSpringService}")
+    private HelloSpringService helloSpringService;
+    
     public String showHello() {
-        return "Hello from managed bean";
+        return helloSpringService.sayHello();
     }
+
+    public void setHelloSpringService(HelloSpringService helloSpringService) {
+        this.helloSpringService = helloSpringService;
+    }
+    
+
 }
