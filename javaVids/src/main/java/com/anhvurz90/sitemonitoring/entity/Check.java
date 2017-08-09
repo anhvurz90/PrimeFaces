1e588ce2eed3164461bc1e38262d93fdb5b7a35a
anhvurz90 <anhvurz90@gmail.com>
2017-08-09 17:28:53 +0700

    Add JavaVids: JSF & PrimeFaces & Spring tutorial 15: JSF Validation With Hibernate Validator

--- javaVids/src/main/java/com/anhvurz90/sitemonitoring/entity/Check.java
+++ javaVids/src/main/java/com/anhvurz90/sitemonitoring/entity/Check.java
@@ -3,6 +3,9 @@ package com.anhvurz90.sitemonitoring.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,8 +19,11 @@ public class Check {
     @GeneratedValue
     private Integer id;
     
+    @Size(min = 1, message = "Name can not be empty!")
     private String name;
     
+    @Size(min = 1, message = "Url can not be empty!")
+    @URL(message = "Invalid url!")
     private String url;
     
 }
