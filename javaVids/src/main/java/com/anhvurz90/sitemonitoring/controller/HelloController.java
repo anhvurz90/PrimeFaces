0c77d30ab67a0f894363a6c86fe5f44e35e8ce8c
anhvurz90 <anhvurz90@gmail.com>
2017-08-09 15:41:38 +0700

    Add JavaVids: JSF & PrimeFaces & Spring tutorial 6: ManagedBean vs Component vs Named

--- javaVids/src/main/java/com/anhvurz90/sitemonitoring/controller/HelloController.java
+++ javaVids/src/main/java/com/anhvurz90/sitemonitoring/controller/HelloController.java
@@ -2,13 +2,24 @@ package com.anhvurz90.sitemonitoring.controller;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import com.anhvurz90.sitemonitoring.service.HelloSpringService;
 
-@ManagedBean
+//@ManagedBean
+@Scope("request")
+//@Component
+@Named
 public class HelloController {
 
-    @ManagedProperty("#{helloSpringService}")
+//    @ManagedProperty("#{helloSpringService}")
+//    @Autowired
+    @Inject
     private HelloSpringService helloSpringService;
     
     public String showHello() {
